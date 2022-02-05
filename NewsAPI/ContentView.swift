cc05a38d0f689aa67f27ea6067bd817d814d52bc
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2022-02-05 17:47:56 +0100

    Initial Commit

---
+++ NewsAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsAPI
+//
+//  Created by Jose Manuel Ortiz Sanchez on 5/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Jose Manuel Ortiz Sanchez on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
